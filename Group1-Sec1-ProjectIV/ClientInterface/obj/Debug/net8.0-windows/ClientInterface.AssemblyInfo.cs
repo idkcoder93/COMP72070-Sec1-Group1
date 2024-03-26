1898c2685373aee57f4ca8bb3d24aa0310ea6360
alexfridman <afridman0793@conestogac.on.ca>
2024-03-26 15:33:58 -0400

    changes on json files and dll commiting

--- Group1-Sec1-ProjectIV/ClientInterface/obj/Debug/net8.0-windows/ClientInterface.AssemblyInfo.cs
+++ Group1-Sec1-ProjectIV/ClientInterface/obj/Debug/net8.0-windows/ClientInterface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientInterface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef7e8bac43318b9eee62fd586eb90f90a0340f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3615d81dd0a23bf684a8465d570c51e880f3b6b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientInterface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientInterface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
